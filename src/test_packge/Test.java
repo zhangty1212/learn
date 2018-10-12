83cd3f4b7f8fa1f5f02f95ded6693330c1af552e
zhangty1212 <2547260515@qq.com>
2018-10-12 19:21:41 +0800

    新增代码包 testst_packge

---
+++ src/test_packge/Test.java
@@ -0,0 +1,16 @@
+package test_packge;
+/**
+* @author: 张涛一  
+* @E-mail: 2547260515@qq.com
+* @version 创建时间：2018年10月12日 下午7:19:02
+* 类说明
+*/
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 System.out.println("哈哈哈！");
+	}
+
+}
